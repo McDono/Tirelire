f1ceb6e0717e7187775e92a671b4e047e8ecd4b5
Donovan <mcdonovan@outlook.fr>
2018-07-25 19:33:57 +0200

    ajoute le control du servo, reprendre ligne 62 pour la comfirmation du mdp

--- V1.0/Tirelire_arduino/Tirelire_arduino.ino
+++ V1.0/Tirelire_arduino/Tirelire_arduino.ino
@@ -1,6 +1,15 @@
 #include <Keypad.h>
 #include <Servo.h>
 
+/*-------------NOTE----------------
+ * rajouter option de delais de l'ouverture en fonction de la lettre entrée (en dernière entrée)
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
 //KEYPAD
 const byte rows = 4;
 const byte cols = 4;
@@ -17,36 +26,61 @@ Keypad keypad = Keypad(makeKeymap(keys), rowPins, colsPins, rows, cols);
 //SERVO
 Servo myServo;
 const byte servoPin = 10;
-
+const byte angleInit = 0; //A definir. Angle de départ du servo moteur
+const byte angleOuverture = 80; //Angle d'ouverture de la porte
 //Mot de passe
-const String mdp = "419A";
+const String mdp = "419";
 String buffer[4] = "FFFF";
-byte k = 0; //variable permettant le remplissage initial du 
+boolean doorOpen = false; //true = porte ouverte, false = porte fermée
+//byte k = 0; //variable permettant le remplissage initial du 
 
 void setup()
 {
   Serial.begin(9600);
   myServo.attach(servoPin);
+  myServo.write(angleInit);
 }
 
 void loop()
 {
   char key = keypad.getKey();
-  if (buffer[3] == "F") //Tant que le buffer n'est pas remplie, on remplie le buffer
-  {
-    buffer[k] = key;
-  }
-  else
-  {
-    for (byte i = 0; i < mdp.length(); i++)
-  }
+  
   if (key != NO_KEY)
+  {
     Serial.println(key);
-  
-  
-  
-  
-  
+    if (buffer[3] == "F") //Tant que le buffer n'est pas remplie, on remplie le buffer
+    {
+      buffer[k] = key;
+    }
+    else //Si le buffer est plein, on décale toute les lettres et la nouvelle entrée est mise en dernière position du buffer
+    {
+      for (byte i = 0; i < mdp.length()-1; i++)
+        buffer[i] = buffer[i+1];
+      buffer[mdp.lenght()] = key;
+    }
+
+    if (buffer.subString(0,2) == mdp && doorOpen == true)
+    {
+      myServo.write(angleInit + angleOuverture)
+      doorOpen = true;
+      if (buffer[3] == "A")
+      {
+        delay(15000);
+        myServo.write(angleInit)
+      }
+
+    if (doorOpen == true && key == "D")
+    {
+      myServo.write(angleInit)
+      doorOpen = false;
+  }
 }
+
+checkCode(String buffer)
+{
+  if (
+}
+
+
 
 
